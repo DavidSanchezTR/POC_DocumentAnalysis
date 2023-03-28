54152f36bfa7c9ce8f8945c67a188c485f554c18
DavidSanchezTR <david.sanchezfuentes@thomsonreuters.com>
2023-03-28 12:40:21 +0200

    [Se modifica en context

--- Aranzadi.DocumentAnalysis.Data/Entities/DocumentAnalysisDbContext.cs
+++ Aranzadi.DocumentAnalysis.Data/Entities/DocumentAnalysisDbContext.cs
@@ -1,12 +1,13 @@
 ﻿
 using Aranzadi.DocumentAnalysis.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 namespace Aranzadi.DocumentAnalysis.Data
 {
     public class DocumentAnalysisDbContext : DbContext
     {
-        //public DbSet<DocumentAnalysisData> DocumentAnalysisData => Set<DocumentAnalysisData>();
+        public DbSet<DocumentAnalysisData> Analysis => Set<DocumentAnalysisData>();        
 
         public DocumentAnalysisDbContext(DbContextOptions<DocumentAnalysisDbContext> options)
             : base(options) 
@@ -15,8 +16,18 @@ namespace Aranzadi.DocumentAnalysis.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder.Entity<DocumentAnalysisData>().HasPartitionKey(x => x.NewGuid).Property(y => y.Analisis).HasConversion(
+                        j => ToJson(j),
+                        j => FromJson<string>(j)
+            );
             base.OnModelCreating(builder);            
-        } 
-        
+        }
+        public static string ToJson<T>(T item) => JsonSerializer.Serialize(item);
+        public static T? FromJson<T>(string json) => JsonSerializer.Deserialize<T>(json);
+        public string? GetCosmosContainerName(Type type)
+        {
+            var entityType = Model.FindEntityType(type);
+            return entityType?.GetContainer();
+        }
     }
 }
