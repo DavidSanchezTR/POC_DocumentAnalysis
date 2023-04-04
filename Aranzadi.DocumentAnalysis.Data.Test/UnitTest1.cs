2a8cd5b11042dd97f29127f0a6b746a4a1374562
DavidSanchezTR <david.sanchezfuentes@thomsonreuters.com>
2023-04-04 15:41:58 +0200

    Creación de proyectos Test

---
+++ Aranzadi.DocumentAnalysis.Data.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aranzadi.DocumentAnalysis.Data.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Aranzadi.DocumentAnalysis.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aranzadi.DocumentAnalysis.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
